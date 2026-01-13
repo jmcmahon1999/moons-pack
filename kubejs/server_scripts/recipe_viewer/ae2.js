ac0a0221f3ab67a13c3e287accfac77fe41488d9
Joe McMahon <jmcmahon1999@gmail.com>
2026-01-13 11:39:16 +0000

    chore: added some visual mods, draconic evolution config, and recipe viewer kubejs scripts

---
+++ kubejs/server_scripts/recipe_viewer/ae2.js
@@ -0,0 +1,4 @@
+RecipeViewerEvents.removeEntries('item', e => {
+    e.remove('ae2:inscriber')
+    e.remove('ae2:silicon')
+})
---
+++ kubejs/server_scripts/recipe_viewer/railcraft.js
@@ -0,0 +1,6 @@
+RecipeViewerEvents.removeEntries('item', e => {
+    e.remove('railcraft:bushing_gear')
+    e.remove('railcraft:coal_dust')
+    e.remove('railcraft:charcoal_dust')
+    e.remove('railcraft:ender_dust')
+})
