e817b72b09a3e9b350908a4989e91d28fdd6ff35
Joe <jmcmahon1999@gmail.com>
2026-03-18 22:34:38 +0000

    fix: made continuity optional.

---
+++ kubejs/server_scripts/moons_pack/loot/aether.js
@@ -0,0 +1,8 @@
+LootJS.lootTables(event => {
+    event.removeGlobalModifiers("aether:gloves_loot_chain")
+    event.removeGlobalModifiers("aether:gloves_loot_diamond")
+    event.removeGlobalModifiers("aether:gloves_loot_gold")
+    event.removeGlobalModifiers("aether:gloves_loot_iron")
+    event.removeGlobalModifiers("aether:gloves_loot_leather")
+    event.removeGlobalModifiers("aether:gloves_loot_netherite")
+})
