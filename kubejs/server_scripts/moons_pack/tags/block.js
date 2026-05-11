e49b96d7900d3c5073fe519276104492933c508b
Joe <jmcmahon1999@gmail.com>
2026-05-11 22:08:36 +0100

    chore: work on black hole multiblock

---
+++ kubejs/server_scripts/moons_pack/tags/block.js
@@ -0,0 +1,8 @@
+ServerEvents.tags('block', e => {
+    e.add("oritech:blackhole_blacklist", "minecraft:bedrock")
+    e.add("oritech:blackhole_blacklist", "#dimdoors:fabric")
+    e.add("oritech:blackhole_blacklist", "#dimdoors:ancient_fabric")
+    e.add("oritech:blackhole_blacklist", "dimdoors:unravelled_fabric")
+    e.add("oritech:blackhole_blacklist", "dimdoors:solid_static")
+    e.add("oritech:blackhole_blacklist", "#modular_machinery_reborn:casing")
+})
