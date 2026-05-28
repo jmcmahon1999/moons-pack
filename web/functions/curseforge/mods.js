091a294d2a08ba8555d0623927dc59fa2e3963eb
Joe McMahon <jmcmahon1999@gmail.com>
2026-05-28 15:02:30 +0100

    docs: fixed broken webpage modlist

--- web/functions/curseforge/mods.js
+++ web/functions/curseforge/mods.js
@@ -1,31 +1,10 @@
 export async function onRequestGet({ request, env }) {
-  /*const origin = request.headers.get("Origin");
-  const referer = request.headers.get("Referer");
-
-  const allowedOrigin = env.CF_PAGES_URL;
-  const allowedHost = allowedOrigin.split("//")[-1];
-
-  const refererOk = (() => {
-    if (!referer) return false;
-    try {
-      return new URL(referer).host === allowedHost;
-    } catch {
-      return false;
-    }
-  })();
-
-  if (origin !== allowedOrigin && !refererOk) {
-    if (env.WORKER_ENV != "local") {
-      return new Response("[Moon-Pack] Forbidden", { status: 403, statusText: "[Moon-Pack] Forbidden" });
-    }
-  }*/
   const url = new URL(request.url);
   const query = url.searchParams.get("ids");
 
   if (!query) {
     return new Response("Missing ids", { status: 400 });
   }
-
   const upstream = await fetch(`https://api.curseforge.com/v1/mods`, {
     method: 'POST',
     body: JSON.stringify({
--- web/functions/curseforge/versions.js
+++ web/functions/curseforge/versions.js
@@ -1,24 +1,4 @@
 export async function onRequestGet({ request, env }) {
-  /*const origin = request.headers.get("Origin");
-  const referer = request.headers.get("Referer");
-
-  const allowedOrigin = env.CF_PAGES_URL;
-  const allowedHost = allowedOrigin.split("//")[-1];
-
-  const refererOk = (() => {
-    if (!referer) return false;
-    try {
-      return new URL(referer).host === allowedHost;
-    } catch {
-      return false;
-    }
-  })();
-
-  if (origin !== allowedOrigin && !refererOk) {
-    if (env.WORKER_ENV != "local") {
-      return new Response("[Moons-Pack] Forbidden", { status: 403 });
-    }
-  }*/
   const url = new URL(request.url);
   const query = url.searchParams.get("ids");
 
