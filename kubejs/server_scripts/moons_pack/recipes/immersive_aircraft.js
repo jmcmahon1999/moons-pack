5f4320f41277c8927f14714a5675be187a20acc9
Joe <jmcmahon1999@gmail.com>
2026-06-14 15:42:56 +0100

    fix: added stack group lang entries. tweaked immersive aircraft recipes. added clay and sand deposits to worldgen.

---
+++ kubejs/server_scripts/moons_pack/recipes/immersive_aircraft.js
@@ -0,0 +1,21 @@
+ServerEvents.recipes(e => {
+    e.replaceInput({mod: "immersive_aircraft"},
+        "immersive_aircraft:propeller",
+        "modern_industrialization:aluminium_rotor"
+    )
+
+    e.replaceInput({mod: "immersive_aircraft"},
+        "immersive_aircraft:engine",
+        "modern_industrialization:motor"
+    )
+
+    e.replaceInput({mod: "immersive_aircraft"},
+        "minecraft:lever",
+        "modern_industrialization:piston"
+    )
+
+    e.replaceInput({mod: "immersive_aircraft"},
+        "minecraft:comparator",
+        "modern_industrialization:analog_circuit"
+    )
+})
