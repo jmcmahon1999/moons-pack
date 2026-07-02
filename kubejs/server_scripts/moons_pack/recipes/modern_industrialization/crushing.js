43e912b54e329e02312c416b5bbcf8ad5fa740aa
Joe McMahon <jmcmahon1999@gmail.com>
2026-07-02 22:17:30 +0100

    fix: tuff recipe.

---
+++ kubejs/server_scripts/moons_pack/recipes/modern_industrialization/crushing.js
@@ -0,0 +1,15 @@
+ServerEvents.recipes(e => {
+  e.custom({
+    "type": "modern_industrialization:macerator",
+    "eu": 2,
+    "duration": 100,
+    "item_inputs": {
+      "item": "minecraft:cobbled_deepslate",
+      "amount": 1
+    },
+    "item_outputs": {
+      "item": "minecraft:tuff",
+      "amount": 1
+    }
+  })
+})
